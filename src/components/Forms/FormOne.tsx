513a57b8baaf4f225cb65ccec89e8b625a0ca7d9
AbdulkarimOgaji <2010704039@udusok.edu.ng>
2022-04-30 06:43:03 +0100

    Replaced validation with formik and yup

--- src/components/Forms/FormOne.tsx
+++ src/components/Forms/FormOne.tsx
@@ -8,10 +8,26 @@ import { RootState } from '../../app/store'
 import { useSelector, useDispatch } from 'react-redux'
 import { setEmail, setEmailErrors } from '../../features/user/userSlice'
 import { validateEmail, anyError } from "./validations";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import ErrorBox from "../ErrorBox";
+import * as Yup from 'yup'
+import { useSearchParams } from "react-router-dom";
 
+
+const validationSchema = Yup.object({
+  email: Yup.string().required("This field is required").email("Must provide a valid email Address")
+})
+const initialState = {
+  email: ""
+}
 
 const FormOne = () => {
+  const [searchParams, setSearchParams] = useSearchParams()
+  const handleSubmit = () => {
+    console.log("Submitted");
+  setSearchParams({ form_id: '2'});
+    
+  }
   // const [email, setEmail] = useState("")
   // const [emailErr, setEmailErr] = useState({} as myValidation)
   const emailErr = useSelector((state: RootState) => state.user.loginErrors.email)
@@ -23,26 +39,33 @@ const FormOne = () => {
     dispatch(setEmailErrors(err))
   };
   return (
-    <Box>
-      <Typography
-        variant="h5"
-        color="secondary"
-        fontWeight="bold"
-        textAlign="center"
-      >
-        Email Address
-      </Typography>
-      <Stack spacing={3} p={3}>
-        <TextField
-          variant="standard"
-          label="Email Address"
-          error={anyError(emailErr)}
-          required
-          onChange={handleChange}
-        ></TextField>
-        <ErrorBox validation={emailErr} />
-      </Stack>
-    </Box>
+    // <Box>
+    //   <Typography
+    //     variant="h5"
+    //     color="secondary"
+    //     fontWeight="bold"
+    //     textAlign="center"
+    //   >
+    //     Email Address
+    //   </Typography>
+    //   <Stack spacing={3} p={3}>
+    //     <TextField
+    //       variant="standard"
+    //       label="Email Address"
+    //       error={anyError(emailErr)}
+    //       required
+    //       onChange={handleChange}
+    //     ></TextField>
+    //     <ErrorBox validation={emailErr} />
+    //   </Stack>
+    // </Box>
+    <Formik onSubmit={handleSubmit} validationSchema={validationSchema} initialValues={initialState}>
+      <Form>
+        <Field type="email" name="email" placeholder="email" />
+        <ErrorMessage name="email"/>
+        <input type="submit" id="formSubmit" />
+      </Form>
+    </Formik>
   );
 };
 export default FormOne;
