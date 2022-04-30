f0e3e78e314262ae65d07de68c9bcab6736bc878
AbdulkarimOgaji <2010704039@udusok.edu.ng>
2022-04-30 10:04:31 +0100

    implement loginAction redux

--- src/components/Forms/FormThree.tsx
+++ src/components/Forms/FormThree.tsx
@@ -1,25 +1,38 @@
 import { Box, Typography } from "@mui/material";
-import { Navigate, useNavigate, useSearchParams } from "react-router-dom";
-import { Formik, Field, Form, ErrorMessage } from "formik";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import { loginRequest, UserState } from "../../features/user/userSlice";
+import { Formik, Field, Form, ErrorMessage, FormikValues } from "formik";
 import * as Yup from "yup";
 
-const initialValues = {};
+const initialValues = {
+  password: '',
+  password2: ''
+};
 const validationSchema = Yup.object({
-  password: Yup.string().required("THis field is requied").min(6),
-  password2: Yup.string().required("THis field is requied"),
+  password: Yup.string().required("This field is requied").min(6),
+  password2: Yup.string().required("This field is requied"),
 });
 
 const FormThree = () => {
+  const dispatch = useAppDispatch();
+  const { isLoggedIn, loginError } = useAppSelector((state) => state.user);
   const [searchParams, setSearchParams] = useSearchParams();
-  const prevParams = Object.fromEntries([...searchParams])
-  const navigate = useNavigate()
+  const prevParams = Object.fromEntries([...searchParams]);
+  const navigate = useNavigate();
   const goToPrevious = () => {
-    setSearchParams({...prevParams, form_id: '2'}, {replace: true})
-  }
+    setSearchParams({ ...prevParams, form_id: "2" }, { replace: true });
+  };
 
-  const handleSubmit = () => {
-    navigate('/')
-  }
+  const handleSubmit = (values: FormikValues) => {
+    const payload = { ...prevParams, password: values.password };
+    dispatch(loginRequest(payload as UserState["data"]));
+    if (isLoggedIn) {
+      navigate("/");
+    } else {
+      alert(loginError);
+    }
+  };
   return (
     <Box>
       <Typography
@@ -31,7 +44,13 @@ const FormThree = () => {
       >
         Setup Password
       </Typography>
-      <input type="submit" className="submit-hidden" id="previousBtn" onClick={goToPrevious} value="" />
+      <input
+        type="submit"
+        className="submit-hidden"
+        id="previousBtn"
+        onClick={goToPrevious}
+        value=""
+      />
       <Formik
         onSubmit={handleSubmit}
         initialValues={initialValues}
