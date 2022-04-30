0ddbf77dcfaa4255b859657947112ebc720451e7
AbdulkarimOgaji <2010704039@udusok.edu.ng>
2022-04-30 06:50:21 +0100

    delete old validation code

--- src/components/Forms/FormOne.tsx
+++ src/components/Forms/FormOne.tsx
@@ -1,15 +1,4 @@
-import {
-  Box,
-  Typography,
-  Stack,
-  TextField,
-} from "@mui/material";
-import { RootState } from '../../app/store'
-import { useSelector, useDispatch } from 'react-redux'
-import { setEmail, setEmailErrors } from '../../features/user/userSlice'
-import { validateEmail, anyError } from "./validations";
 import { Formik, Form, Field, ErrorMessage } from "formik";
-import ErrorBox from "../ErrorBox";
 import * as Yup from 'yup'
 import { useSearchParams } from "react-router-dom";
 
@@ -24,41 +13,10 @@ const initialState = {
 const FormOne = () => {
   const [searchParams, setSearchParams] = useSearchParams()
   const handleSubmit = () => {
-    console.log("Submitted");
   setSearchParams({ form_id: '2'});
     
   }
-  // const [email, setEmail] = useState("")
-  // const [emailErr, setEmailErr] = useState({} as myValidation)
-  const emailErr = useSelector((state: RootState) => state.user.loginErrors.email)
-  const dispatch = useDispatch()
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    dispatch(setEmail(e.target.value))
-    var err = validateEmail(e.target.value);
-    dispatch(setEmailErrors(err))
-  };
   return (
-    // <Box>
-    //   <Typography
-    //     variant="h5"
-    //     color="secondary"
-    //     fontWeight="bold"
-    //     textAlign="center"
-    //   >
-    //     Email Address
-    //   </Typography>
-    //   <Stack spacing={3} p={3}>
-    //     <TextField
-    //       variant="standard"
-    //       label="Email Address"
-    //       error={anyError(emailErr)}
-    //       required
-    //       onChange={handleChange}
-    //     ></TextField>
-    //     <ErrorBox validation={emailErr} />
-    //   </Stack>
-    // </Box>
     <Formik onSubmit={handleSubmit} validationSchema={validationSchema} initialValues={initialState}>
       <Form>
         <Field type="email" name="email" placeholder="email" />
--- src/components/Forms/FormThree.tsx
+++ src/components/Forms/FormThree.tsx
@@ -1,57 +1,34 @@
-import { Box, Typography, Stack, Input } from "@mui/material";
-import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "../../app/store";
-import ErrorBox from "../ErrorBox";
-import { setPassword, setPasswordErrs } from "../../features/user/userSlice";
 import { useSearchParams } from "react-router-dom";
-import { Formik, Field, Form, ErrorMessage } from "formik"
-import * as Yup from 'yup'
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
-
-
-const initialValues = {
-
-}
+const initialValues = {};
 const validationSchema = Yup.object({
   password: Yup.string().required("THis field is requied").min(6),
-  password2: Yup.string().required("THis field is requied")
-})
+  password2: Yup.string().required("THis field is requied"),
+});
 
 const FormThree = () => {
-  const [searchParams, setSearchParams] = useSearchParams()
+  const [searchParams, setSearchParams] = useSearchParams();
 
   const handleSubmit = () => {
-    setSearchParams({form_id:"4"})
-  }
-  const password = useSelector((state: RootState) => state.user.password)
-  const passwordErrs = useSelector((state: RootState) => state.user.loginErrors.password)
-  const dispatch = useDispatch()
-  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    var val = e.currentTarget.value
-    dispatch(setPassword(val))
-  }
-
-  const handleConfirmation = (e: React.ChangeEvent<HTMLInputElement>) => {
-    var val = e.currentTarget.value
-    var validity = [
-      {
-        clause: "Password must be more than 6 characters",
-        valid: password.length > 6
-      },
-      {
-        clause: "Password must Match",
-        valid: val === password
-      }
-    ]
-    dispatch(setPasswordErrs(validity))
-  }
+    setSearchParams({ form_id: "4" });
+  };
 
   return (
-    <Formik onSubmit={handleSubmit} initialValues={initialValues} validationSchema={validationSchema}>
+    <Formik
+      onSubmit={handleSubmit}
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+    >
       <Form>
         <Field name="password" placeholder="password" type="password" />
         <ErrorMessage name="password" />
-        <Field name="password2" placeholder="password confirmation" type="password" />
+        <Field
+          name="password2"
+          placeholder="password confirmation"
+          type="password"
+        />
         <ErrorMessage name="password2" />
         <input type="submit" id="formSubmit" />
       </Form>
